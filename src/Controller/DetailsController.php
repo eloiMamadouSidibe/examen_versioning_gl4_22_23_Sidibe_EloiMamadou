93d66bb8092f45806b5b7bb20e51b935ec03ada1
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-13 02:24:24 +0000

    Display for product details from model with link update

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailsController extends AbstractController
 {
-    #[Route('/details', name: 'app_details')]
-    public function index(): Response
+    private $entityManager;
+
+    function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+    
+    #[Route('/details/{slug}', name: 'app_details')]
+    public function index($slug): Response
+    {
+        $categories = $this->entityManager->getRepository(Category::class)->findAll();
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        if(!$product) return $this->redirectToRoute('app_home');
+        
         return $this->render('details/index.html.twig', [
-            'controller_name' => 'DetailsController',
+            'categories' => $categories,
+            'product' => $product,
         ]);
     }
 }
