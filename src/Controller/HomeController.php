ea368bd07e1c939fcd66b57f71d6679520097678
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-13 01:04:55 +0000

    Display for categories in home from model

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $categories = $this->entityManager->getRepository(Category::class)->findAll();
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 }
